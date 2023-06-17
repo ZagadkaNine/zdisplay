50503850c54a0ce9282c7196efc2bf6b294a2c4e
zagadkanine <zagadkanine@dynamic-foundries.com>
2023-06-17 13:59:45 +0200

    WIP

--- roles/system_configuration/scripts/lid_status.py
+++ roles/system_configuration/scripts/lid_status.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 
 import re
+import os
+import syslog   
 
 def getLidState():
-    f = open("/proc/acpi/button/lid/LID0/state", 'r')
-    lidState = re.sub(r"state:[\n\t\s]+", "", f.read()).rstrip()
-    f.close()
-    return lidState
+    lidPath = "/proc/acpi/button/lid/LID0/state"
+    if os.path.exists(lidPath):
+        f = open(lidPath, "r")
+        lidState = re.sub(r"state:[\n\t\s]+", "", f.read()).rstrip()
+        f.close()
+        return lidState
+    return "unknown"
+
 
 if __name__ == "__main__":
     print("{}".format(getLidState()))
--- roles/system_configuration/scripts/manage.py
+++ roles/system_configuration/scripts/manage.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 
-from lid_status import getLidState
+import syslog
+import re
+import os
+
+def getLidState():
+    lidPath = "/proc/acpi/button/lid/LID0/state"
+    if os.path.exists(lidPath):
+        f = open(lidPath, "r")
+        lidState = re.sub(r"state:[\n\t\s]+", "", f.read()).rstrip()
+        f.close()
+        return lidState
+    return "unknown"
 
 if __name__ == "__main__":
+    syslog.syslog(syslog.LOG_INFO, "Lid state: {}".format(getLidState()))
     print("{}".format(getLidState()))
 
 
