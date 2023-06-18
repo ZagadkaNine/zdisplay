a91aafae0f067400182025233c689af8ed0d0cc3
zagadkanine <zagadkanine@dynamic-foundries.com>
2023-06-18 08:37:10 +0200

    WIP

--- roles/system_configuration/scripts/manage.py
+++ roles/system_configuration/scripts/manage.py
@@ -3,8 +3,10 @@
 import syslog
 import re
 import os
+from Xlib import X, display, Xutil
+from Xlib.ext import randr
 
-def getLidState():
+def get_lid_state():
     lidPath = "/proc/acpi/button/lid/LID0/state"
     if os.path.exists(lidPath):
         f = open(lidPath, "r")
@@ -13,8 +15,23 @@ def getLidState():
         return lidState
     return "unknown"
 
+def get_active_randr_outputs():
+    d = display.Display()
+    s = d.screen()
+    window = s.root.create_window(0, 0, 1, 1, 1, s.root_depth)
+    res = randr.get_screen_resources(window)
+    outputs = []
+    for output in res.outputs:
+        info = randr.get_output_info(window, output, 0)
+        if info.connection == 0:
+            print(info)
+            outputs.append(info.name)
+    return outputs
+
 if __name__ == "__main__":
-    syslog.syslog(syslog.LOG_INFO, "Lid state: {}".format(getLidState()))
-    print("{}".format(getLidState()))
+    syslog.syslog(syslog.LOG_INFO, "Lid state: {}".format(get_lid_state()))
+    print("{}".format(get_lid_state()))
 
-
+    
+    displays_names = get_active_randr_outputs() 
+    print("{}".format(displays_names))
